d3cfb26001963dfa05b76930005cc41867beff0e
Farzam Ahmad <farzam135@gmail.com>
2021-12-11 03:17:43 -0500

    change timeout on waiting for selector

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const { chromium } = require("playwright");
   page.once('load', () => console.log('Page loaded!'))
   await page.goto('https://netflix.com').catch(err => console.log(err));
   await page.click('text=Sign In');
-  await page.fill('input[name="userLoginId"]', 'username');
+  await page.fill('input[name="userLoginId"]', 'email@email.com');
   await page.fill('input[name="password"]', 'password');
-  await page.click('text=Sign In');
+  await page.click('button[type="submit"]');
+  const ele = await page.waitForSelector('b', {visible: true, timeout: 2000}).catch(err => console.log('Valid credentials'));
+  if (ele) console.log('Error, invalid credential!');
   await page.screenshot({path: 'screenshot.png'});
   await browser.close();
   // YOUR CODE ENDS
